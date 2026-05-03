a77b75931e66dde9a9ed274eec7e0cb00a78042c
lyonsyonii <liam@garriga.dev>
2026-05-03 11:15:45 +0200

    first

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            pnpm
+            nodejs
+            typescript
+            angular-language-server
+          ];
+
+          buildInputs = with pkgs; [
+          ];
+
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath nativeBuildInputs;
+        };
+      }
+    );
+}

d347bf9dbbda6534e8ff5a43f18e1441878734bd
lyonsyonii <liam@garriga.dev>
2026-05-04 18:54:50 +0200

    add pwa and gh pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeuix/themes/aura";
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
+import { provideServiceWorker } from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,12 +16,16 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          prefix: 'p',
+          prefix: "p",
           darkModeSelector: ".dark-mode",
           // darkModeSelector: ".dark-mode",
         },
       },
     }),
     provideClientHydration(withEventReplay()),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 };
